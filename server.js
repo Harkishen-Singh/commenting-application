306338c11336df79b2fc593b50fad669b0684c9a
Harkishen-Singh <harkishensingh@hotmail.com>
2019-10-04 01:37:52 +0530

    first version

---
+++ server.js
@@ -0,0 +1,91 @@
+/* eslint-disable no-undef */
+const app = require('express')(),
+	parser = require('body-parser'),
+	port = process.env.PORT || 5000,
+	url = '0.0.0.0',
+	JsonDB = require('node-json-db').JsonDB,
+	Config = require('node-json-db/dist/lib/JsonDBConfig').Config;
+
+var db = new JsonDB(new Config('database/comments-store', true, true, '/'));
+
+app.use(parser.json());
+
+app.use(parser.urlencoded({
+	extended:true,
+}));
+
+app.use((_,res,next)=>{
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
+app.get('/', (_, res) => {
+	// respond to the request ping
+	res.send('pong');
+});
+
+app.post('/post-comment', (req, res) => {
+	let cmt = req.body.comment,
+		time = req.body.time,
+		name = req.body.name;
+	let dataDB = db.getData('/comments'),
+		len = Object.keys(dataDB).length;
+
+	db.push('/comments/' + String(len + 1), {
+		'name': name,
+		'message': cmt,
+		'id': len + 1,
+		'upvotes': 0,
+		'downvotes': 0,
+		'time': time
+	});
+	dataDB = db.getData('/comments');
+	let arr = [];
+	for (let inst in dataDB) {
+		arr.push(dataDB[inst]);
+	}
+	res.send(arr);
+});
+
+app.post('/upvote-comment', (req, res) => {
+	let id = req.body.id;
+	let dataDB = db.getData('/comments/' + String(id));
+	dataDB['upvotes'] = parseInt(dataDB['upvotes']) + 1;
+	db.delete('/comments/' + String(id));
+	db.push('/comments/' + String(id), dataDB);
+	dataDB = db.getData('/comments');
+	let arr = [];
+	for (let inst in dataDB) {
+		arr.push(dataDB[inst]);
+	}
+	res.send(arr);
+});
+
+app.post('/downvote-comment', (req, res) => {
+	let id = req.body.id;
+	let dataDB = db.getData('/comments/' + String(id));
+	dataDB['downvotes'] = parseInt(dataDB['downvotes']) + 1;
+	db.delete('/comments/' + String(id));
+	db.push('/comments/' + String(id), dataDB);
+	dataDB = db.getData('/comments');
+	let arr = [];
+	for (let inst in dataDB) {
+		arr.push(dataDB[inst]);
+	}
+	res.send(arr);
+});
+
+app.get('/comments', (req, res) => {
+	let dataDB = db.getData('/comments'),
+		arr = [];
+	for (let inst in dataDB) {
+		arr.push(dataDB[inst]);
+	}
+	res.send(arr);
+});
+
+const server = app.listen(port, url, err => {
+	if (err) throw err;
+	else   console.warn('Up and running at ', server.address().address, ' :', server.address().port);
+});
--- ui/app.js
+++ ui/app.js
@@ -1,20 +1,98 @@
+// eslint-disable-next-line no-undef
 var app = angular.module('cmt-application', ['ngRoute']);
 
 var global = {
-    // URL for backend service listening at :5000
-    url:'http://127.0.0.1:5000',
-    username: 'test',
+	// URL for backend service listening at :5000
+	url: 'http://127.0.0.1:5000',
+	username: 'test',
 };
 
-app.config(function($routeProvider,$locationProvider) {
-    $routeProvider
-    .when("/",{
-        templateUrl:'./components/article.html',
-        controller:'general-controller',
-        title:'Login | SignUp',
-    })
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/',{
+			templateUrl:'./components/article.html',
+			controller:'comments-controller',
+			title:'Login | SignUp',
+		});
 });
 
-app.controller('general-controller', function($scope,$location,$rootScope,$http) {
-    console.log('general conreoller called')
+app.controller('comments-controller', function($scope,$location,$rootScope,$http) {
+	$scope.comment = '';
+
+	$scope.submitComment = function() {
+		console.log('comment is ', $scope.comment, ' ', $scope.name);
+		if ($scope.comment.length !== 0) {
+			let date = new Date();
+			let time = date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear();
+			time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ' ' + time;
+			$http({
+				url: global.url + '/post-comment',
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				},
+				data: 'comment=' + $scope.comment + '&name=' + $scope.name + '&time=' + time
+			}).then(resp => {
+				let res = resp.data;
+				// sort in decending order
+				res.sort((a, b) => {
+					return b['id']-a['id'];
+				});
+				$scope.comments = res;
+			});
+		} else {
+			alert('please write a comment');
+		}
+	};
+
+	$scope.upvoteComment = function(id) {
+		console.log('id upvote is ', id);
+		$http({
+			url: global.url + '/upvote-comment',
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			data: 'id=' + id
+		}).then(resp => {
+			let res = resp.data;
+			res.sort((a, b) => {
+				return b['id']-a['id'];
+			});
+			$scope.comments = res;
+		});
+	};
+
+	$scope.downvoteComment = function(id) {
+		$http({
+			url: global.url + '/downvote-comment',
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			data: 'id=' + id
+		}).then(resp => {
+			let res = resp.data;
+			res.sort((a, b) => {
+				return b['id']-a['id'];
+			});
+			$scope.comments = res;
+		});
+	};
+
+	$scope.getComments = function() {
+		$http({
+			url: global.url + '/comments',
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
+		}).then(resp => {
+			let res = resp.data;
+			res.sort((a, b) => {
+				return b['id']-a['id'];
+			});
+			$scope.comments = res;
+		});
+	};
 });
